9a522371a3bc90e1df8769639d7d0c92dae8616f
Sohelrana2815 <sohel152302@gmail.com>
2025-02-05 10:35:00 +0600

    start making a tic-tac-toe game

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+const App = () => {
+  // State for game board
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  // State to track whose turn it is
+  const [xIsNext, setXIsNext] = useState(true);
+
+  // Function to check for a winner
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8], // Rows
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8], // Columns
+      [0, 4, 8],
+      [2, 4, 6], // Diagonals
+    ];
+  };
+
+  return <div></div>;
+};
+
+export default App;
